670a9191ce7bc41cd36c2cc51251b5ff7d612433
austinatkinson93 <austinatkinson93@users.noreply.github.com>
2020-04-30 20:53:26 -0400

    fixed some small issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login"
 import userProfilePage from "./pages/UserProfile"
@@ -11,21 +10,20 @@ import Signup from './components/SignUp';
 function App() {
 
   return (
-
-   <Signup />
-
-    <Router>
-    
-      <div>
-        <Navigation />
-        <Switch>
-          <Route exact path={["/userProfile"]} component={userProfilePage}></Route>
-          <Route exact path={["/userPetsProfile"]} component={PetsProfilePage}></Route>
-          <Route exact path={["/adminPage"]} component={Admin}></Route>
-          <Route exact path={["/", "/Chipper"]} component={Login}></Route>
-        </Switch>
-      </div>
-    </Router >
+    <div>
+      <Signup />
+      <Router>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route exact path={["/userProfile"]} component={userProfilePage}></Route>
+            <Route exact path={["/userPetsProfile"]} component={PetsProfilePage}></Route>
+            <Route exact path={["/adminPage"]} component={Admin}></Route>
+            <Route exact path={["/", "/Chipper"]} component={Login}></Route>
+          </Switch>
+        </div>
+      </Router >
+    </div>
 
   );
 }
