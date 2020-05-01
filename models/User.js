71132ddaab44b2750f3f2540a85182c221ae3019
John Sasser <cw.john.sasser@gmail.com>
2020-04-30 20:19:18 -0400

    packages, server, components, images, models, routers

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+ 
+const User = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      password: {
+        type: String,
+        required: true
+      }
+});
+ 
+User.plugin(passportLocalMongoose);
+ 
+module.exports = mongoose.model('User', User);
--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+const cors = require("cors")
+const apiRoutes = require('./routes/index')
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 app.use(express.static("client"));
+
+app.use(cors())
+
+app.use(apiRoutes)
+
 
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/index.html"));
 });
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/chipperdb");
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
