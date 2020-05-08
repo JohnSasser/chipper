3be1f1433abdabe702410d8411a75dd32a78eff7
Alan Margolies <amargolies88@gmail.com>
2020-05-07 22:17:31 -0400

    removed long req log

--- routes/petApi.js
+++ routes/petApi.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const db = require("../models");
 
 router.get('/api/pets', function (req, res) {
-    console.log("REACHED API/PETS", req);
+    console.log("REACHED API/PETS", req.user);
     db.Pets.find()
         .then(pets => {
             res.status(200).json(pets);
