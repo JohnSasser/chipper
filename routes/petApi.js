afebc6f9c645e52334a5c41ee437f2579f2d2156
austinatkinson93 <austinatkinson93@users.noreply.github.com>
2020-05-07 22:04:57 -0400

    pets form complete

--- routes/petApi.js
+++ routes/petApi.js
@@ -12,26 +12,22 @@ router.get('/api/pets', function (req, res) {
 
 router.post('/api/add', function (req, res) {
     console.log("REACHED API/add");
-    
+
     let newPet = {
         petName: req.body.petName,
         microNum: req.body.microNum,
         species: req.body.species
     }
 
-    db.Pets.create(newPet, (err, result) => {
+    db.Pets.insertMany(newPet, (err, result) => {
         console.log("reached inside model")
         if (err) {
-          res.send(err);
+            res.send(err);
         } else {
-          console.log(result);
-          res.send(result);
+            console.log(result);
+            res.send(result);
         }
-      })
-        .then(pets => {
-            res.status(200).json(pets);
-        })
-        .catch(err => console.log(err));
+    })
 })
 
 module.exports = router;
