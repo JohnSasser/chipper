92aa5f264e6e39fa479d11f9f9fc33bf5bf85f54
ramiforde <ramiforde@gmail.com>
2020-05-15 18:45:01 -0400

    Added text-area

--- client/src/components/Feed/index.js
+++ client/src/components/Feed/index.js
@@ -3,8 +3,17 @@ import React from "react";
 function Feed() {
   return (
     <div className="container">
-    <h1>Alert the community about a lost pet</h1>;
-    </div>
+      <h1 className="text-center">Create a post</h1>
+      <h3 className="text-center">Let us know you have lost or found a pet</h3>
+    <br/>
+    <div className="form-group">
+    <label for="exampleFormControlTextarea1">Title:</label>
+    <input type="title" className="form-control" id="title"></input>
+    <label for="exampleFormControlTextarea1">Body:</label>
+    <textarea className="form-control" id="text-body" rows="3"></textarea>
+    <button type="submit" className="btn btn-outline-dark">Submit</button>
+  </div>
+  </div>
   )
 }
 
---
+++ models/post.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// const passportLocalMongoose = require('passport-local-mongoose');
+
+const postSchema = new Schema({
+  title: {
+    type: String,
+    required: false,
+    trim: true
+  },
+  body:{
+      type: String,
+      required: false,
+      trim: true
+  }
+});
+
+// User.plugin(passportLocalMongoose);
+
+
+module.exports = mongoose.model("post", postSchema);
