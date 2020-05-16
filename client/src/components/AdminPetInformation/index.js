e1cf00b6a2b7a3cb38024c7b961715114df52b30
John Sasser <cw.john.sasser@gmail.com>
2020-05-15 23:05:29 -0400

    Admin page login and information

--- client/src/components/AdminPetInformation/index.js
+++ client/src/components/AdminPetInformation/index.js
@@ -1,23 +1,19 @@
 import React from "react";
+import "./style.css"
 
 function AdminPetInformation(props) {
   return (
-    <div>
-      <div className="row">
-        <div className="card" style={{ width: 18 + "rem" }}>
-          <img
-            src="https://placedog.net/70/40"
-            className="card-img-top"
-            alt="..."
-          ></img>
-          <div className="card-body">
-            <h5 className="card-title">{props.petName}</h5>
-            <p className="card-text">Microchip Number: {props.microNum}</p>
-            <button className="btn btn-primary">
-              Possible Link to Records
-            </button>
-          </div>
-        </div>
+    
+    <div className="card owner-card">
+      <img
+        src={props.pupImage}
+        className="card-img-top background-img"
+        alt="dog image"
+      ></img>
+      <div className="card-body">
+        <h5 className="card-title">{props.petName}</h5>
+        <p className="card-text">Microchip Number: {props.microNum}</p>
+        <button className="btn btn-primary">Possible Link to Records</button>
       </div>
     </div>
   );
