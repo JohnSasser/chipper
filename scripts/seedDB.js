e4203e94142f7bfd17c79e9d5942add188a876e1
Alan Margolies <amargolies88@gmail.com>
2020-06-02 14:46:44 -0400

    init usable seedDB.js and require dotenv where mongoose is used in scripts files

---
+++ scripts/seedDB.js
@@ -0,0 +1,24 @@
+require('dotenv').config;
+let db = require("../models");
+let mongoose = require("mongoose");
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://DBadmin:g0atf4c3@ds017185.mlab.com:17185/heroku_7s6d98z5",
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+    }
+);
+
+db.adminkeys
+    .deleteMany({})
+    .then(() => db.adminkeys.collection.insertMany(adminkeys))
+    .then((data) => {
+        // console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
