d404614ee9fc81acfbd848fe5a21188a94f9535d
John Sasser <cw.john.sasser@gmail.com>
2020-06-02 22:45:38 -0400

    removed the search bar from feed

--- client/src/pages/AllPostPage/index.js
+++ client/src/pages/AllPostPage/index.js
@@ -9,18 +9,13 @@ function AllPosts() {
   // current user for the user check;
   const { user } = useContext(AuthContext);
 
-  // use effect for res.data === user from /authenticated to reroute home if user is not logged in ****
-  // useEffect(() => {
-
-  // }, []);
-
   return (
     <div>
       <UserDirectory />
       <div className="container">
         <div className="row">
           <div className="col-md-12 col-lg-12 col-sm-12">
-            <Search />
+            {/* <Search /> */}
             <ViewAllPosts />
           </div>
         </div>
